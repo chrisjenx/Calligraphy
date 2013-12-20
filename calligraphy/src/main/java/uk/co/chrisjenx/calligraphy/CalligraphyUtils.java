ff2ffcfbbc9c63e29b3dff8061e10f21d7316d7d
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2013-12-20 01:28:20 +0000

    Util methods to set Typeface to text

--- calligraphy/src/main/java/uk/co/chrisjenx/calligraphy/CalligraphyUtils.java
+++ calligraphy/src/main/java/uk/co/chrisjenx/calligraphy/CalligraphyUtils.java
@@ -1,10 +1,27 @@
 package uk.co.chrisjenx.calligraphy;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
 /**
  * Created by chris on 20/12/2013
  * Project: Calligraphy
  */
 public final class CalligraphyUtils {
+
+    public static final void applyFontToTextView(final TextView textView, final Typeface typeface) {
+        if (textView == null || typeface == null) return;
+        textView.setTypeface(typeface);
+    }
+
+    public static final void applyFontToTextView(final Context context, final TextView textView, final String filePath) {
+        if(textView == null) return;
+        final AssetManager assetManager = context.getAssets();
+        final Typeface typeface = TypefaceUtils.load(assetManager, filePath);
+        applyFontToTextView(textView, typeface);
+    }
 
     private CalligraphyUtils() {
     }
---
+++ calligraphy/src/main/java/uk/co/chrisjenx/calligraphy/TypefaceUtils.java
@@ -0,0 +1,47 @@
+package uk.co.chrisjenx.calligraphy;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.util.Log;
+
+import java.util.Hashtable;
+
+/**
+ * A helper loading {@link android.graphics.Typeface} avoiding the leak of the font when loaded
+ * by multiple calls to {@link android.graphics.Typeface#createFromAsset(android.content.res.AssetManager, String)}
+ * on pre-ICS versions.
+ * <p/>
+ * More details can be found here https://code.google.com/p/android/issues/detail?id=9904
+ * <p/>
+ * Created by Chris Jenkins on 04/09/13.
+ */
+public final class TypefaceUtils {
+
+    private static Hashtable<String, Typeface> sCachedFonts = new Hashtable<String, Typeface>();
+
+    /**
+     * A helper loading a custom font.
+     *
+     * @param assetManager App's asset manager.
+     * @param filePath     The path of the file.
+     * @return Return {@link android.graphics.Typeface} or null if the path is invalid.
+     */
+    public static Typeface load(final AssetManager assetManager, final String filePath) {
+        synchronized (sCachedFonts) {
+            try {
+                if (!sCachedFonts.contains(filePath)) {
+                    final Typeface typeface = Typeface.createFromAsset(assetManager, filePath);
+                    sCachedFonts.put(filePath, typeface);
+                    return typeface;
+                }
+            } catch (Exception e) {
+                Log.w("Calligraphy", "Can't create asset from " + filePath + ". Make sure you have passed in the correct path and file name.", e);
+                return null;
+            }
+            return sCachedFonts.get(filePath);
+        }
+    }
+
+    private TypefaceUtils() {
+    }
+}

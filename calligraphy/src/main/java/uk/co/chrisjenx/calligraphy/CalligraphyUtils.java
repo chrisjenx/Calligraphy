00fed432a967a85973c6280b5dc805a9290c1ebb
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2013-12-20 01:18:12 +0000

    font and util class

---
+++ calligraphy/src/main/java/uk/co/chrisjenx/calligraphy/CalligraphyUtils.java
@@ -0,0 +1,11 @@
+package uk.co.chrisjenx.calligraphy;
+
+/**
+ * Created by chris on 20/12/2013
+ * Project: Calligraphy
+ */
+public final class CalligraphyUtils {
+
+    private CalligraphyUtils() {
+    }
+}

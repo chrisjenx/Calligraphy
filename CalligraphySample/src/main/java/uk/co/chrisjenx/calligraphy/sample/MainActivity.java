aa0bcb62f130c0eac48ea86036eda8a880f19d63
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2014-04-28 22:22:41 +0100

    Added Toast test to show custom text

--- CalligraphySample/src/main/java/uk/co/chrisjenx/calligraphy/sample/MainActivity.java
+++ CalligraphySample/src/main/java/uk/co/chrisjenx/calligraphy/sample/MainActivity.java
@@ -9,7 +9,10 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -40,9 +43,21 @@ public class MainActivity extends FragmentActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
             return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            ButterKnife.inject(this, view);
+        }
+
+        @OnClick({R.id.button_default, R.id.button_bold})
+        public void onClickButton() {
+            Toast.makeText(getActivity(), "Custom Typeface toast text", Toast.LENGTH_SHORT).show();
         }
     }
 }

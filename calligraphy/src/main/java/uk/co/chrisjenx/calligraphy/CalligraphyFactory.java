67c4d62287511d24bd03b65365269642569fb611
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2015-12-07 22:22:20 +0000

    huge clean up moveing to as v2

--- calligraphy/src/main/java/uk/co/chrisjenx/calligraphy/CalligraphyFactory.java
+++ calligraphy/src/main/java/uk/co/chrisjenx/calligraphy/CalligraphyFactory.java
@@ -12,8 +12,8 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
-import java.lang.reflect.Method;
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Method;
 
 class CalligraphyFactory {
 
@@ -153,59 +153,6 @@ class CalligraphyFactory {
             toolbar.getViewTreeObserver().addOnGlobalLayoutListener(new ToolbarLayoutListener(this, context, toolbar));
         }
 
-    }
-
-    private static class ToolbarLayoutListener implements ViewTreeObserver.OnGlobalLayoutListener {
-
-        private final WeakReference<CalligraphyFactory> mCalligraphyFactory;
-        private final WeakReference<Context> mContextRef;
-        private final WeakReference<Toolbar> mToolbarReference;
-        private final CharSequence orignalTitle;
-        private final CharSequence orignalSubTitle;
-
-        private ToolbarLayoutListener(final CalligraphyFactory calligraphyFactory,
-                                      final Context context, Toolbar toolbar) {
-            mCalligraphyFactory = new WeakReference<>(calligraphyFactory);
-            mContextRef = new WeakReference<>(context);
-            mToolbarReference = new WeakReference<>(toolbar);
-            orignalTitle = toolbar.getTitle();
-            orignalSubTitle = toolbar.getSubtitle();
-            toolbar.setTitle(" ");
-            toolbar.setSubtitle("  ");
-        }
-
-        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-        @Override public void onGlobalLayout() {
-            final Toolbar toolbar = mToolbarReference.get();
-            final Context context = mContextRef.get();
-            final CalligraphyFactory factory = mCalligraphyFactory.get();
-            if (toolbar == null) return;
-            if (factory == null || context == null) {
-                removeSelf(toolbar);
-                return;
-            }
-
-            int childCount = toolbar.getChildCount();
-            if (childCount != 0) {
-                // Process children, defer draw as it has set the typeface.
-                for (int i = 0; i < childCount; i++) {
-                    factory.onViewCreated(toolbar.getChildAt(i), context, null);
-                }
-            }
-            removeSelf(toolbar);
-            toolbar.setTitle(orignalTitle);
-            toolbar.setSubtitle(orignalSubTitle);
-        }
-
-        private void removeSelf(final Toolbar toolbar) {// Our dark deed is done
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                //noinspection deprecation
-                toolbar.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-            } else {
-                toolbar.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-            }
-        }
-
         // Try to set typeface for custom views using interface method or via reflection if available
         if (view instanceof HasTypeface) {
             Typeface typeface = getDefaultTypeface(context, resolveFontPath(context, attrs));
@@ -220,8 +167,9 @@ class CalligraphyFactory {
                 ReflectionUtils.invokeMethod(view, setTypeface, typeface);
             }
         }
-    }
 
+    }
+    
     private Typeface getDefaultTypeface(Context context, String fontPath) {
         if (TextUtils.isEmpty(fontPath)) {
             fontPath = CalligraphyConfig.get().getFontPath();
@@ -251,4 +199,58 @@ class CalligraphyFactory {
 
         return textViewFont;
     }
+
+    private static class ToolbarLayoutListener implements ViewTreeObserver.OnGlobalLayoutListener {
+
+        private final WeakReference<CalligraphyFactory> mCalligraphyFactory;
+        private final WeakReference<Context> mContextRef;
+        private final WeakReference<Toolbar> mToolbarReference;
+        private final CharSequence originalTitle;
+        private final CharSequence originalSubTitle;
+
+        private ToolbarLayoutListener(final CalligraphyFactory calligraphyFactory,
+                                      final Context context, Toolbar toolbar) {
+            mCalligraphyFactory = new WeakReference<>(calligraphyFactory);
+            mContextRef = new WeakReference<>(context);
+            mToolbarReference = new WeakReference<>(toolbar);
+            originalTitle = toolbar.getTitle();
+            originalSubTitle = toolbar.getSubtitle();
+            toolbar.setTitle(" ");
+            toolbar.setSubtitle("  ");
+        }
+
+        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+        @Override public void onGlobalLayout() {
+            final Toolbar toolbar = mToolbarReference.get();
+            final Context context = mContextRef.get();
+            final CalligraphyFactory factory = mCalligraphyFactory.get();
+            if (toolbar == null) return;
+            if (factory == null || context == null) {
+                removeSelf(toolbar);
+                return;
+            }
+
+            int childCount = toolbar.getChildCount();
+            if (childCount != 0) {
+                // Process children, defer draw as it has set the typeface.
+                for (int i = 0; i < childCount; i++) {
+                    factory.onViewCreated(toolbar.getChildAt(i), context, null);
+                }
+            }
+            removeSelf(toolbar);
+            toolbar.setTitle(originalTitle);
+            toolbar.setSubtitle(originalSubTitle);
+        }
+
+        private void removeSelf(final Toolbar toolbar) {// Our dark deed is done
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                //noinspection deprecation
+                toolbar.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            } else {
+                toolbar.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        }
+
+    }
+
 }
